4f02cde0577ea5d3dda9400efb3cd2be8abb065b
MELODY SIMWA <simwamelody@gmail.com>
2024-11-29 15:29:52 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+// Declare customerName in the global scope
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'some initial value';
+
+
+//function to modify customerName
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+
+
+function overwriteBestCustomer(){
+  bestCustomer='maybe bob';  
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='new value';
+}
